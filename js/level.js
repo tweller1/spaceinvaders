d40052b50171a6ead16346ac7a2c7923e37ac4f6
Tess Weller <tess_weller@hotmail.co.uk>
2014-05-13 10:33:23 +0100

    Initial Commit

---
+++ js/level.js
@@ -0,0 +1,75 @@
+//which space invaders are present on each level
+  var levelData = { 
+     1:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,2,2,0,0,0,0],
+          [0,0,0,0,0,1,1,0,0,0,0]],
+     2:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0]] };
+
+
+//places aliens on the canvas and the frames for the srpite sheet
+  var spriteData = {
+    'alien1': { sx: 0,  sy: 0,  w: 23, h: 18, cls: Alien, frames: 3 },
+    'alien2': { sx: 0,  sy: 18, w: 23, h: 18, cls: Alien, frames: 3 },
+    'player': { sx: 0,  sy: 36, w: 26, h: 17, cls: Player },
+    'missile': { sx: 0,  sy: 86, w: 3,  h: 14, cls: Missile }
+  }
+
+//introduction to the game, wording
+  function startGame() {
+    var screen = new GameScreen("Aliens have Invaded","press space to start",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+    Game.loop();
+  }
+
+//wording at the end of the game - if lost
+  function endGame() {
+    var screen = new GameScreen("Game Over","(press space to restart)",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+  }
+
+//wording at the end of the game - if won
+  function winGame() {
+    var screen = new GameScreen("You Won!","(press space to restart)",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+  }
+
+//sound fire (shot by player) sound die (hit invaders)
+  $(function() {
+    GameAudio.load({ 'fire' : 'media/62362__fons__zap-1.wav', 'die' : 'media/34205__themfish__zoup.wav    ' }, 
+                   function() { 
+                       Game.initialize("#gameboard", levelData, spriteData,
+                                      { "start": startGame,
+                                        "die"  : endGame,
+                                        "win"  : winGame });
+                   });
+   });
+
+
+
